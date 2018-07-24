b98d53cc538961a1a3488c01b24882101f1ecb1c
Ramon <pcnay2003@gmail.com>
2018-07-24 09:56:52 -0700

    Se reorganiza las carpetas de las carpetas de los Tutoriales

---
+++ Curso_Udemy/ASP.NET-Basico-Angel/MiPrimeraAplicacion/MiPrimeraAplicacion/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Mi primera Aplicacion en ASP.NET MVC 5 ";
+        }
+    }
+}
---
+++ Curso_Udemy/ASP_NET_MVC-Core-JuanCalos/JuanCarlosArcila/Programa1/Programa1/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    public class Categoria
+    {
+    }
+}
