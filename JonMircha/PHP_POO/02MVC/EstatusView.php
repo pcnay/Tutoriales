c9c3e79169379d5396a6c45139ca4daf5a07bc60
Ramon <pcnay2003@gmail.com>
2018-11-04 13:23:39 -0800

    Se termina de mostrar la tabla estatus en la seccion de vistas, es provicional

--- JonMircha/PHP_POO/02MVC/EstatusView.php
+++ JonMircha/PHP_POO/02MVC/EstatusView.php
@@ -8,6 +8,28 @@
   // para mostrar lo que retorna la consulta en el arreglo que se asigno.
   // $estatus->read();
   // Ahora se manda un parámetro.
-  $datosEstatus = $estatus->read(3);
-  var_dump($datosEstatus);
+  $datosEstatus = $estatus->read(3); // Enlace de Modelo con la Vista, no es la forma adecuada mas adelante de adaptara correctamente.
+  // var_dump($datosEstatus);
+  // Obteniendo el número total de registros del arreglo.
+  $num_estatus = count($datosEstatus);
+
+  echo '<h2>Número total de elementos del arreglo <mark>'.$num_estatus.'</mark></h2>';
+  echo "<h2>Número total de elementos del arreglo <mark> $num_estatus </mark></h2>";
+
+  echo '<h2>Tabla de Estatus </h2>';
+  echo '<table> 
+          <tr>
+            <th>estatus_id</th>
+            <th>estatus</th>                        
+          </tr>';
+  // Se desplegara el contenido del arrelgo.
+          for ($n=0; $n<$num_estatus; $n++)
+          {
+            echo '<tr>
+                    <td>'.$datosEstatus[$n]['estatus_id'].'</td>
+                    <td>'.$datosEstatus[$n]['estatus'].'</td>                        
+                  </tr>';
+          };
+  echo  '</table>';
+
 ?>
