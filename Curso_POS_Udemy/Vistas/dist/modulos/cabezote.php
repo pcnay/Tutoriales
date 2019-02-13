d25dc62d49c14c7fe0bfb8a52a98dc3d7db081a0
Ramon <pcnay2003@gmail.com>
2019-02-12 21:39:39 -0800

    Se agrega el encabezado y menu general del sistema POS

--- Curso_POS_Udemy/Vistas/dist/modulos/cabezote.php
+++ Curso_POS_Udemy/Vistas/dist/modulos/cabezote.php
@@ -5,21 +5,46 @@
     <!-- Logo mini -->
     <span class = "logo-mini">
       <img src = "vistas/img/plantilla/icono-blanco.png" class = "img-responsive" style = "padding:10px"> 
+      <!-- "img-responsive" = Es una clase de Bootstrap para responsive-->
+
     </span>
+  
 
     <!-- Logo normal -->
-    <span class = "logo-mini">
+    <span class = "logo-lg">
       <img src = "vistas/img/plantilla/logo-blanco-lineal.png" class = "img-responsive" style = "padding:10px 0px"> 
     </span>
 
-    <!-- Barra de navegación . 
+    <!-- Barra de navegación .  < Side Bar >
       Es que muestra el menu en fomra de libros aun lado del logo de Casco Construcción.
     -->
-    <nav>
+    <!-- Boton de navegación (símbolo de libros)-->
+    <nav class = "navbar navbar-static-top" role="navigation">
       <a href="#" class = "sidebar-toggle" data-toggle ="push-menu" role ="button">
         <span class="sr-only">Toggle navigation</span>
-
+        
       </a>
+      <!-- Perfil dde usuario-->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class= "dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle = "dropdown">
+              <img src="vistas/img/usuarios/default/anonymous.png" class="user-image">
+              <span class="hidden-xs">Usuario Administrador</span>
+            </a>
+            <!-- Dropdown-toggle, barras de desplazamiento.-->
+            <ul class="dropdown-menu"> 
+              <li class="user-body">
+                <div class="pull-right">
+                  <a href=""class="btn btn-default btn-flat">Salir</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
     </nav>
+    
   </a>
 </header>
