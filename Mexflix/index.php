39196600b970f8932ae730d31334eb574e12ee2e
Ramon <pcnay2003@gmail.com>
2019-02-26 22:17:04 -0800

    Otra prueba de proyecto diferente

--- Mexflix/index.php
+++ Mexflix/index.php
@@ -4,6 +4,7 @@
   // Para evitar el error de que no se ha definido la variable, se recurre al uso del operador Ternario.
   // isset() = Para saber si esta definida una variable.
 
+  //
   $autoload = new Autoload();
   $route = isset($_GET['r'] ) ? $_GET['r']:'home' ;
   $mexflix = new Router($route);
