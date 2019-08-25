2ab13aad6458200d557d3ab6f2d1792eb17f8b25
Ramon <pcnay2003@gmail.com>
2019-08-24 20:07:25 -0700

    Buscar Paginador clientes

--- Facturacion/sistema/buscar_cliente.php
+++ Facturacion/sistema/buscar_cliente.php
@@ -2,10 +2,13 @@
   session_start();
   // Para validar los roles de los usuarios.
   // Solo tiene acceso a esta pantalla el Administrador (1)
+  // Para esta opcion todos los usuarios del sistema podran accesar a buscar clientes
+  /*
   if ($_SESSION['rol'] != 1)
   {
     header ("location: ./");
   }
+  */
 
   include "../conexion.php" 
 ?>
@@ -17,7 +20,7 @@
 	<meta charset="UTF-8">
 	<?php include "includes/script.php"; ?>
 
-	<title>Lista de Usuarios</title>
+	<title>Lista de Clientes</title>
 </head>
 <body>
 	<?php include "includes/header.php"; ?>
@@ -26,15 +29,16 @@
       $busqueda = strtolower($_REQUEST['busqueda']);
       if (empty($busqueda))
       {
-        header("location:lista_usuarios.php");
+        header("location:lista_clientes.php");
+        mysqli_close($conexion);
       }
 
     ?>
-    <h1>Lista De Usuarios</h1>
-    <a href = "registro_usuario.php" class = "btn_new">Crear Usuario</a>
+    <h1>Lista De Clientes</h1>
+    <a href = "registro_cliente.php" class = "btn_new">Crear Cliente</a>
     
     <!-- Se agrega el formulario para la busqueda de usuarios. -->
-    <form action="buscar_usuario.php" method="get" class = "form_search">
+    <form action="buscar_cliente.php" method="get" class = "form_search">
       <input type="text" name = "busqueda" id="busqueda" placeholder ="Buscar" value = "<?php echo $busqueda; ?>">
       <input type="submit" value = "Buscar" class = "btn_search">
     </form>
@@ -42,42 +46,29 @@
     <table>
       <tr>
         <th>ID</th>
-        <th>Nombre</th>
-        <th>Correo</th>
-        <th>Usuario</th>
-        <th>Rol</th>
+        <th>NIT</th>
+        <th>NOMBRE</th>
+        <th>TELEFONO</th>
+        <th>DIRECCION</th>
         <th>Acciones</th>
       </tr>
       <?php
-        $rol = '';
-        if ($busqueda == 'administrador')
-        {
-          $rol = " OR rol LIKE '%1%' ";          
-        }
-        else if ($busqueda == 'supervisor')
-        {
-          $rol = " OR rol LIKE '%2%' ";
-        }
-        else if ($busqueda == 'vendedor')
-        {
-          $rol = " OR rol LIKE '%3%' ";
-        }
-
-
-
         // Seccion para el paginador (Barra donde despliega las páginas)
         // Extraer los registros que esten activos
         $sql_registe = mysqli_query($conexion,"SELECT COUNT(*) AS total_registro 
-                                                FROM usuario WHERE  
-                                                (idusuario LIKE '%$busqueda%' OR 
-                                                nombre LIKE '%$busqueda%' OR
-                                                correo LIKE '%$busqueda%' OR
-                                                usuario LIKE '%$busqueda%' $rol) AND 
+                                                FROM cliente WHERE  
+                                                (idcliente LIKE '%$busqueda%' OR 
+                                                nit LIKE '%$busqueda%' OR
+                                                telefono LIKE '%$busqueda%' OR
+                                                direccion LIKE '%$busqueda%') AND 
                                                 estatus = 1");
         
         $result_register = mysqli_fetch_array($sql_registe);
         $total_registro = $result_register['total_registro'];
-        $por_pagina = 5;
+
+        // Los registros a desplegar por pantalla
+        $por_pagina = 3;
+
         if(empty($_GET['pagina']))
         {
           $pagina = 1;
@@ -93,41 +84,40 @@
         $total_paginas = ceil($total_registro/$por_pagina);
 
 
-        // Se obtiene los usuarios con su correspondiente nombre de "Rol" y que tengan en la columna "estatus = 1(Borrado logico)"
+        // Se obtiene los clientes de la columna "estatus = 1(Borrado logico)"
         // Se puede suprimir los campos que no se requiere buscar.
-        $query = mysqli_query($conexion,"SELECT u.idusuario,u.nombre,u.correo,u.usuario,r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE 
-                                          (u.idusuario LIKE '%$busqueda%' OR 
-                                          u.nombre LIKE '%$busqueda%' OR
-                                          u.correo LIKE '%$busqueda%' OR
-                                          u.usuario LIKE '%$busqueda%' OR 
-                                          r.rol LIKE '%$busqueda%') AND 
-                                          u.estatus = 1  ORDER BY u.nombre ASC LIMIT $desde,$por_pagina");
+        $query = mysqli_query($conexion,"SELECT * FROM cliente WHERE 
+                                          (idcliente LIKE '%$busqueda%' OR 
+                                          nit LIKE '%$busqueda%' OR
+                                          nombre LIKE '%$busqueda%' OR
+                                          telefono LIKE '%$busqueda%' OR 
+                                          direccion LIKE '%$busqueda%') AND 
+                                          estatus = 1  ORDER BY nombre ASC LIMIT $desde,$por_pagina");
+        mysqli_close($conexion);
 
         $result = mysqli_num_rows($query);
+
         // Desplegara los datos con la busqueda indicada.
-        
         if ($result > 0)
         {
           while ($data = mysqli_fetch_array($query))
           {
       ?>
             <tr>
-              <td><?php echo $data['idusuario']; ?></td>
+              <td><?php echo $data['idcliente']; ?></td>
+              <td><?php echo $data['nit']; ?></td>
               <td><?php echo $data['nombre']; ?></td>
-              <td><?php echo $data['correo']; ?></td>
-              <td><?php echo $data['usuario']; ?></td>
-              <td><?php echo $data['rol']; ?></td>
+              <td><?php echo $data['telefono']; ?></td>
+              <td><?php echo $data['direccion']; ?></td>
               <td>
-                <a class ="link_edit" href = "editar_usuario.php?id=<?php echo $data['idusuario']; ?>">Editar</a>
-                <?php
-                 if ($data['idusuario'] != 1) 
-                 {
-                ?>
+                <a class ="link_edit" href = "editar_cliente.php?id=<?php echo $data['idcliente']; ?>">Editar</a>
                 |
-                <!-- Si es el usuario = 1 (SuperUsuario) no se puede borrar -->
-                <a class = "link_delete"  href = "eliminar_confirmar_usuario.php?id=<?php echo $data['idusuario']; ?>">Eliminar</a>
-            <?php } ?>
-            
+                <!-- Solo se permite borrar al usuario "Admin", "Supervisor" 
+                 -->
+                <?php if ($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2)  { ?>
+                  <a class = "link_delete"  href = "eliminar_confirmar_cliente.php?id=<?php echo $data['idcliente']; ?>">Eliminar</a>
+                <?php } ?>
+
               </td>
             </tr>
       <?php        
